5f6f31a874f9caf247d5a2d72c7a9099f95fda26
kunal7238 <kunalsingh112311@gmail.com>
2021-11-28 18:34:39 +0530

    remove repetition

--- src/components/Basics/Resturent.js
+++ src/components/Basics/Resturent.js
@@ -2,11 +2,37 @@ import React ,{useState} from "react";
 import "./style.css";
 import Menu from "./menuApi";
 import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
+
+const uniqueList = [...new Set(
+    Menu.map((curElem) => {
+        return curElem.category;
+    })
+),"All",];
+
+console.log(uniqueList);
+
+
 const Resturent = () => {
     const [menuData, SetMenuData] = useState(Menu);
-    console.log(menuData);
+    const [menuList, setMenuList] =useState(uniqueList);
+
+
+    const filterItem = (category) =>{
+
+        if(category === "All"){
+            SetMenuData(Menu);
+            return;
+        }
+
+      const updatedList = Menu.filter((curElem) => {
+          return curElem.category === category;
+      });
+      SetMenuData(updatedList);
+    };
   return (
     <>
+        <Navbar filterItem={filterItem} menuList={menuList}/>
       <MenuCard menuData={menuData}/>
     </>
   );
