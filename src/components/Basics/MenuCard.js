ec8b9644a73b82b1b894858b03a5d6648f18b4af
kunal7238 <kunal.singh_cs19@gla.ac.in>
2022-01-21 13:06:03 +0530

    add redirect file for live in netlify

--- src/components/Basics/MenuCard.js
+++ src/components/Basics/MenuCard.js
@@ -15,7 +15,7 @@ const MenuCard = ({ menuData }) => {
               <span className="card-number card-circle subtle">{id}</span>
               <span className="card-author subtle">{name}</span>
               <h2 className="card-title">{name}</h2>
-              <span className="card-description subtle">
+              <span className="card-description subtle"> 
               {description}
               </span>
               <div className="card-read">Read</div>
